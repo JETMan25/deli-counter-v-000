fae6c568a6d61670adb98601cace17f201e4ab6f
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-30 19:06:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,10 @@ end
 
 
 
-def take_a_number(deli, name)
- deli << name
- puts "Welcome, #{name}. You are number #{katz_deli} in line."
-end
-end
+# def take_a_number(deli, name)
+# deli << name
+# puts "Welcome, #{name}. You are number #{katz_deli} in line."
+# end
+# end
 
 # Write your code here.
